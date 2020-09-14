bf9b838b77994399ed2a0e27fae913d328d340ab
arre1908 <arredoluis@gmail.com>
2020-09-14 14:57:21 -0500

    Added initial files and assets

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <img alt="Vue-SWAPI logo" src="../assets/dstar-128.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>

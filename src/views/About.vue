5c4302a3bf37186e7ceba3cfb1edf5ddbcc50016
arre1908 <arredoluis@gmail.com>
2020-10-21 14:41:37 -0500

    Added meta description and updated about page.

--- src/views/About.vue
+++ src/views/About.vue
@@ -8,16 +8,21 @@
 
     <p>
       This was made possible thanks to the Star Wars API at
-      <a href="https://swapi.dev/" target="_blank">swapi.dev</a>.
-    </p>
-
-    <p>
-      Images obtained from
+      <a href="https://swapi.dev/" target="_blank">swapi.dev</a>. Images
+      obtained from
       <a href="https://github.com/tbone849/star-wars-guide" target="_blank"
         >Star Wars: A Visual Guide</a
       >
       and
       <a href="https://starwars.fandom.com/" target="_blank">Wookiepedia</a>.
+    </p>
+
+    <p>
+      Made by Luis Arredondo. (<a
+        href="https://github.com/arre1908/vue-swapi"
+        target="_blank"
+        >Github</a
+      >)
     </p>
 
     <p></p>
